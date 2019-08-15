22f058d72675c7ab6e4425409fb4c01e3e633b14
Joshua Cummins <cummins-j6@ulster.ac.uk>
2019-08-15 16:20:55 +0100

    Updates

---
+++ precache-manifest.20cf2b56278fd4bd505cc6082251cbbd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3915ba3d727da7d631c4a088776a4fc0",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "dc5e3f5a2284a9b19a2a",
+    "url": "/monster-rolodex/static/css/main.6140559a.chunk.css"
+  },
+  {
+    "revision": "90bc354edad42df017e0",
+    "url": "/monster-rolodex/static/js/2.08bb4953.chunk.js"
+  },
+  {
+    "revision": "dc5e3f5a2284a9b19a2a",
+    "url": "/monster-rolodex/static/js/main.258d9be2.chunk.js"
+  },
+  {
+    "revision": "71d32483bf1c2d8981e2",
+    "url": "/monster-rolodex/static/js/runtime~main.eb8ef101.js"
+  }
+]);
